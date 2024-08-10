3b1a7046f7c781051ddde6ecbb52de103b6f7c52
Selimcan <webisdeveloper@gmail.com>
2024-08-10 09:58:54 +0300

    server side update

--- server/app.js
+++ server/app.js
@@ -2,14 +2,24 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import bookRouter from "./routes/bookRoute.js";
+import cookieParser from "cookie-parser";
+import errorController from "./controller/errorController.js";
+import AppError from "./utils/appError.js";
 
 const app = express();
 
 dotenv.config({ path: "./config/.env" });
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors());
 
 app.use("/api/v1/books", bookRouter);
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`${req.originalUrl} böyle bir sayfa bulunamadı`, 404));
+});
+
+app.use(errorController);
 
 export default app;
--- server/controller/bookController.js
+++ server/controller/bookController.js
@@ -1,19 +1,66 @@
 import Book from "../model/bookModel.js";
+import AppFeature from "../utils/appFeature.js";
 import tryCatch from "../utils/tryCatch.js";
+import AppError from "../utils/appError.js";
 
-export const getAllBook = tryCatch(async (req, res, next) => {
-  const books = await Book.find();
+const getAllBook = tryCatch(async function (req, res, next) {
+  const resultPerPage = 8;
+  const bookCount = await Book.countDocuments();
+  // Executed
+  const apiFeature = new AppFeature(Book.find(), req.query)
+    .search()
+    .filter()
+    .pagination(resultPerPage);
+
+  const books = await apiFeature.query;
+
   res.status(200).json({
-    status: "success",
-    bookLength: books.length,
+    status: true,
+    bookCount,
+    resultPerPage,
     books,
   });
 });
 
-export const createBook = tryCatch(async (req, res, next) => {
+const getOneBook = tryCatch(async function (req, res, next) {
+  const book = await Book.findById(req.params.id);
+  if (!book) {
+    return next(new AppError("Bu ID'e ait bir kitap bulunamadı", 404));
+  }
+  res.status(200).json({
+    status: true,
+    book,
+  });
+});
+
+const createBook = tryCatch(async function (req, res, next) {
   const newBook = await Book.create(req.body);
   res.status(201).json({
-    status: "success",
+    status: true,
     newBook,
   });
 });
+
+const updateBook = tryCatch(async function (req, res, next) {
+  const book = await Book.findByIdAndUpdate(req.params.id, req.body);
+  if (!book) {
+    return next(new AppError("Bu ID'e ait bir kitap bulunamadı", 404));
+  }
+  res.status(200).json({
+    status: true,
+    book,
+  });
+});
+
+const deleteBook = tryCatch(async function (req, res, next) {
+  const book = await Book.findByIdAndDelete(req.params.id);
+  if (!book) {
+    return next(new AppError("Bu ID'e ait bir kitap bulunamadı", 404));
+  }
+  res.status(204).json({
+    status: true,
+    message: "Book Deleted",
+  });
+});
+
+export { getAllBook, getOneBook, createBook, updateBook, deleteBook };
